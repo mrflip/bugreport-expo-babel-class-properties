811137f3622c14402647f7822f671f6741ee048c
Philip (flip) Kromer <flip@mrflip.com>
2022-09-29 15:23:08 -0500

    chore: added a safe babel config, and a way to test console log and live refresh

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+const favoriteLetter = 'X'
+
 export default function App() {
+
+  console.log(favoriteLetter)
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>This will auto-refresh! And your favorite letter is now {favoriteLetter}</Text>
       <StatusBar style="auto" />
     </View>
   );
--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,21 @@
-module.exports = function(api) {
-  api.cache(true);
+// function getFramework() {
+//   if (process.env.APP_FW) { return process.env.APP_FW }
+//   return (/next$/.test(process.argv[1])) ? 'next' : 'expo'
+// }
+
+// console.warn('is expo?', process.env.EXPO_ENV, Object.keys(process.env).sort(), process.argv)
+
+function ExpoBabelConfig(api) {
+  api.cache(true)
   return {
-    presets: ['babel-preset-expo'],
-  };
-};
+    presets: [
+      ['babel-preset-expo', { lazyImports: true }],
+    ],
+    plugins: [
+      // ["@babel/plugin-proposal-class-properties"],
+      // 'react-native-reanimated/plugin',                 // MUST BE LAST!!!
+    ],
+  }
+}
+
+module.exports = ExpoBabelConfig
